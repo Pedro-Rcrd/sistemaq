e1cc973fdf7c846766d23b2bd95791fed90b022d
Pedro-Rcrd <pedrorlsapalu@gmail.com>
2023-04-24 20:49:23 -0600

    Otros controladores

---
+++ Controllers/EstudiantePatrocinadorsController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using systemquchooch.Models;
+
+namespace systemquchooch.Controllers
+{
+    public class EstudiantePatrocinadorsController : Controller
+    {
+        private readonly QuchoochContext _context;
+
+        public EstudiantePatrocinadorsController(QuchoochContext context)
+        {
+            _context = context;
+        }
+
+        // GET: EstudiantePatrocinadors
+        public async Task<IActionResult> Index()
+        {
+            var quchoochContext = _context.EstudiantePatrocinadors.Include(e => e.CodigoEstudianteNavigation).Include(e => e.CodigoPatrocinadorNavigation);
+            return View(await quchoochContext.ToListAsync());
+        }
+
+        // GET: EstudiantePatrocinadors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.EstudiantePatrocinadors == null)
+            {
+                return NotFound();
+            }
+
+            var estudiantePatrocinador = await _context.EstudiantePatrocinadors
+                .Include(e => e.CodigoEstudianteNavigation)
+                .Include(e => e.CodigoPatrocinadorNavigation)
+                .FirstOrDefaultAsync(m => m.CodigoEstudiantePatrocinador == id);
+            if (estudiantePatrocinador == null)
+            {
+                return NotFound();
+            }
+
+            return View(estudiantePatrocinador);
+        }
+
+        // GET: EstudiantePatrocinadors/Create
+        public IActionResult Create()
+        {
+            ViewData["CodigoEstudiante"] = new SelectList(_context.Estudiantes, "CodigoEstudiante", "CodigoEstudiante");
+            ViewData["CodigoPatrocinador"] = new SelectList(_context.Patrocinadors, "CodigoPatrocinador", "CodigoPatrocinador");
+            return View();
+        }
+
+        // POST: EstudiantePatrocinadors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CodigoEstudiantePatrocinador,CodigoEstudiante,CodigoPatrocinador")] EstudiantePatrocinador estudiantePatrocinador)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(estudiantePatrocinador);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CodigoEstudiante"] = new SelectList(_context.Estudiantes, "CodigoEstudiante", "CodigoEstudiante", estudiantePatrocinador.CodigoEstudiante);
+            ViewData["CodigoPatrocinador"] = new SelectList(_context.Patrocinadors, "CodigoPatrocinador", "CodigoPatrocinador", estudiantePatrocinador.CodigoPatrocinador);
+            return View(estudiantePatrocinador);
+        }
+
+        // GET: EstudiantePatrocinadors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.EstudiantePatrocinadors == null)
+            {
+                return NotFound();
+            }
+
+            var estudiantePatrocinador = await _context.EstudiantePatrocinadors.FindAsync(id);
+            if (estudiantePatrocinador == null)
+            {
+                return NotFound();
+            }
+            ViewData["CodigoEstudiante"] = new SelectList(_context.Estudiantes, "CodigoEstudiante", "CodigoEstudiante", estudiantePatrocinador.CodigoEstudiante);
+            ViewData["CodigoPatrocinador"] = new SelectList(_context.Patrocinadors, "CodigoPatrocinador", "CodigoPatrocinador", estudiantePatrocinador.CodigoPatrocinador);
+            return View(estudiantePatrocinador);
+        }
+
+        // POST: EstudiantePatrocinadors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CodigoEstudiantePatrocinador,CodigoEstudiante,CodigoPatrocinador")] EstudiantePatrocinador estudiantePatrocinador)
+        {
+            if (id != estudiantePatrocinador.CodigoEstudiantePatrocinador)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(estudiantePatrocinador);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EstudiantePatrocinadorExists(estudiantePatrocinador.CodigoEstudiantePatrocinador))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CodigoEstudiante"] = new SelectList(_context.Estudiantes, "CodigoEstudiante", "CodigoEstudiante", estudiantePatrocinador.CodigoEstudiante);
+            ViewData["CodigoPatrocinador"] = new SelectList(_context.Patrocinadors, "CodigoPatrocinador", "CodigoPatrocinador", estudiantePatrocinador.CodigoPatrocinador);
+            return View(estudiantePatrocinador);
+        }
+
+        // GET: EstudiantePatrocinadors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.EstudiantePatrocinadors == null)
+            {
+                return NotFound();
+            }
+
+            var estudiantePatrocinador = await _context.EstudiantePatrocinadors
+                .Include(e => e.CodigoEstudianteNavigation)
+                .Include(e => e.CodigoPatrocinadorNavigation)
+                .FirstOrDefaultAsync(m => m.CodigoEstudiantePatrocinador == id);
+            if (estudiantePatrocinador == null)
+            {
+                return NotFound();
+            }
+
+            return View(estudiantePatrocinador);
+        }
+
+        // POST: EstudiantePatrocinadors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.EstudiantePatrocinadors == null)
+            {
+                return Problem("Entity set 'QuchoochContext.EstudiantePatrocinadors'  is null.");
+            }
+            var estudiantePatrocinador = await _context.EstudiantePatrocinadors.FindAsync(id);
+            if (estudiantePatrocinador != null)
+            {
+                _context.EstudiantePatrocinadors.Remove(estudiantePatrocinador);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EstudiantePatrocinadorExists(int id)
+        {
+          return (_context.EstudiantePatrocinadors?.Any(e => e.CodigoEstudiantePatrocinador == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Controllers/EstudianteTutoriumsController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using systemquchooch.Models;
+
+namespace systemquchooch.Controllers
+{
+    public class EstudianteTutoriumsController : Controller
+    {
+        private readonly QuchoochContext _context;
+
+        public EstudianteTutoriumsController(QuchoochContext context)
+        {
+            _context = context;
+        }
+
+        // GET: EstudianteTutoriums
+        public async Task<IActionResult> Index()
+        {
+            var quchoochContext = _context.EstudianteTutoria.Include(e => e.CodigoEstudianteNavigation).Include(e => e.CodigoTutoriaNavigation);
+            return View(await quchoochContext.ToListAsync());
+        }
+
+        // GET: EstudianteTutoriums/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.EstudianteTutoria == null)
+            {
+                return NotFound();
+            }
+
+            var estudianteTutorium = await _context.EstudianteTutoria
+                .Include(e => e.CodigoEstudianteNavigation)
+                .Include(e => e.CodigoTutoriaNavigation)
+                .FirstOrDefaultAsync(m => m.CodigoEstudianteTutoria == id);
+            if (estudianteTutorium == null)
+            {
+                return NotFound();
+            }
+
+            return View(estudianteTutorium);
+        }
+
+        // GET: EstudianteTutoriums/Create
+        public IActionResult Create()
+        {
+            ViewData["CodigoEstudiante"] = new SelectList(_context.Estudiantes, "CodigoEstudiante", "CodigoEstudiante");
+            ViewData["CodigoTutoria"] = new SelectList(_context.Tutoria, "CodigoTutoria", "CodigoTutoria");
+            return View();
+        }
+
+        // POST: EstudianteTutoriums/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CodigoEstudianteTutoria,CodigoEstudiante,CodigoTutoria")] EstudianteTutorium estudianteTutorium)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(estudianteTutorium);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CodigoEstudiante"] = new SelectList(_context.Estudiantes, "CodigoEstudiante", "CodigoEstudiante", estudianteTutorium.CodigoEstudiante);
+            ViewData["CodigoTutoria"] = new SelectList(_context.Tutoria, "CodigoTutoria", "CodigoTutoria", estudianteTutorium.CodigoTutoria);
+            return View(estudianteTutorium);
+        }
+
+        // GET: EstudianteTutoriums/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.EstudianteTutoria == null)
+            {
+                return NotFound();
+            }
+
+            var estudianteTutorium = await _context.EstudianteTutoria.FindAsync(id);
+            if (estudianteTutorium == null)
+            {
+                return NotFound();
+            }
+            ViewData["CodigoEstudiante"] = new SelectList(_context.Estudiantes, "CodigoEstudiante", "CodigoEstudiante", estudianteTutorium.CodigoEstudiante);
+            ViewData["CodigoTutoria"] = new SelectList(_context.Tutoria, "CodigoTutoria", "CodigoTutoria", estudianteTutorium.CodigoTutoria);
+            return View(estudianteTutorium);
+        }
+
+        // POST: EstudianteTutoriums/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CodigoEstudianteTutoria,CodigoEstudiante,CodigoTutoria")] EstudianteTutorium estudianteTutorium)
+        {
+            if (id != estudianteTutorium.CodigoEstudianteTutoria)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(estudianteTutorium);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EstudianteTutoriumExists(estudianteTutorium.CodigoEstudianteTutoria))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CodigoEstudiante"] = new SelectList(_context.Estudiantes, "CodigoEstudiante", "CodigoEstudiante", estudianteTutorium.CodigoEstudiante);
+            ViewData["CodigoTutoria"] = new SelectList(_context.Tutoria, "CodigoTutoria", "CodigoTutoria", estudianteTutorium.CodigoTutoria);
+            return View(estudianteTutorium);
+        }
+
+        // GET: EstudianteTutoriums/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.EstudianteTutoria == null)
+            {
+                return NotFound();
+            }
+
+            var estudianteTutorium = await _context.EstudianteTutoria
+                .Include(e => e.CodigoEstudianteNavigation)
+                .Include(e => e.CodigoTutoriaNavigation)
+                .FirstOrDefaultAsync(m => m.CodigoEstudianteTutoria == id);
+            if (estudianteTutorium == null)
+            {
+                return NotFound();
+            }
+
+            return View(estudianteTutorium);
+        }
+
+        // POST: EstudianteTutoriums/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.EstudianteTutoria == null)
+            {
+                return Problem("Entity set 'QuchoochContext.EstudianteTutoria'  is null.");
+            }
+            var estudianteTutorium = await _context.EstudianteTutoria.FindAsync(id);
+            if (estudianteTutorium != null)
+            {
+                _context.EstudianteTutoria.Remove(estudianteTutorium);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EstudianteTutoriumExists(int id)
+        {
+          return (_context.EstudianteTutoria?.Any(e => e.CodigoEstudianteTutoria == id)).GetValueOrDefault();
+        }
+    }
+}
