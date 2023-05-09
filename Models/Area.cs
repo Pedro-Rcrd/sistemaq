d79c8439e3080f293370304c3348163803567dbd
Pedro-Rcrd <pedrorlsapalu@gmail.com>
2023-05-08 22:29:38 -0600

    agregando display name al archivo de area

--- Models/Area.cs
+++ Models/Area.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace systemquchooch.Models;
 
 public partial class Area
 {
+    [DisplayName("Código de área")]
     public int CodigoArea { get; set; }
 
+    [DisplayName("Nombre")]
     public string Nombre { get; set; } = null!;
 
+
+    [DisplayName("Descripción")]
     public string Descripcion { get; set; } = null!;
 
     public virtual ICollection<Tutorium> Tutoria { get; set; } = new List<Tutorium>();
