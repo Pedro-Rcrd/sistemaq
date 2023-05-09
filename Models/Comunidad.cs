faac358627cee160803352cd8e912f0541a16acf
BartoloLacan <bartolome1997@gmail.com>
2023-05-08 23:27:23 -0600

    Subir comunidad

--- Models/Comunidad.cs
+++ Models/Comunidad.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace systemquchooch.Models;
 
 public partial class Comunidad
 {
+    [DisplayName("Código de Comunidad")]
     public int CodigoComunidad { get; set; }
-
+    [DisplayName("Nombre")]
     public string NombreComunidad { get; set; } = null!;
 
     public virtual ICollection<Estudiante> Estudiantes { get; set; } = new List<Estudiante>();
